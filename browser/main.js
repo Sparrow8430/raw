1bfc2183209d1f3349b99494b20f99e84b02d719
Sparrow8430 <puresociety@protonmail.com>
2025-11-08 01:12:33 -0600

    Update main.js

--- browser/main.js
+++ browser/main.js
@@ -1,25 +1,31 @@
 const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
+        title: "Pure Searx",
         webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
+            nodeIntegration: false, // turn off Node in renderer for security
+            contextIsolation: true, // isolate context
         }
     });
 
-    win.loadFile('index.html');
+    // Load your local SearxNG server
+    win.loadURL('http://127.0.0.1:8888');
+
+    // Optional: prevent navigation outside your server
+    win.webContents.on('will-navigate', (event, url) => {
+        if (!url.startsWith('http://127.0.0.1:8888')) {
+            event.preventDefault();
+        }
+    });
 }
 
-app.whenReady().then(() => {
-    createWindow();
+app.whenReady().then(createWindow);
 
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow();
-    });
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
 app.on('window-all-closed', () => {
